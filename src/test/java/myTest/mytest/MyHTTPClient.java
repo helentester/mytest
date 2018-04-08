812d3882e56772a3097c0870757f8bc085c63afb
helentester163com <helentester163com@192.168.31.205>
2018-04-08 23:00:12 +0800

    添加httpclient

---
+++ src/test/java/myTest/mytest/MyHTTPClient.java
@@ -0,0 +1,39 @@
+/**
+ * @author:Helen
+ * @date：2018年4月8日
+ * @Description: 
+ */
+package myTest.mytest;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+
+public class MyHTTPClient {
+
+    public static void main(String[] args) throws Exception {
+    	CloseableHttpClient httpCilent = HttpClients.createDefault();//Creates CloseableHttpClient instance with default configuration.
+    	HttpGet httpGet = new HttpGet("http://www.baidu.com");
+    	String srtResult = "";
+    	try {
+    		HttpResponse httpResponse = httpCilent.execute(httpGet);
+    	    srtResult = EntityUtils.toString(httpResponse.getEntity());//获得返回的结果
+            System.out.println(srtResult);
+    	} catch (IOException e) {
+    	    e.printStackTrace();
+    	}finally {
+    	    try {
+    	        httpCilent.close();//释放资源
+    	    } catch (IOException e) {
+    	        e.printStackTrace();
+    	    }
+    	}
+    }
+}
+
+
