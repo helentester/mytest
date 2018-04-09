cc6c3b4ee0dd1d95a18696b522844d76a1f4dea0
helentester <helentester@163.com>
2018-04-09 16:25:45 +0800

    加入HTTP接口测试相关方法

--- src/test/java/myTest/mytest/MyHTTPClient.java
+++ src/test/java/myTest/mytest/MyHTTPClient.java
@@ -1,24 +1,24 @@
+package myTest.mytest;
+
+import java.io.IOException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 /**
  * @author:Helen
  * @date：2018年4月8日
  * @Description: 
  */
-package myTest.mytest;
-
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
-
 public class MyHTTPClient {
 
     public static void main(String[] args) throws Exception {
     	CloseableHttpClient httpCilent = HttpClients.createDefault();//Creates CloseableHttpClient instance with default configuration.
-    	HttpGet httpGet = new HttpGet("http://www.baidu.com");
+    	HttpGet httpGet = new HttpGet("http://spring.dev.xsteach.com/api/member/logout");
+    	HttpPost httpPost = new HttpPost("api.dev.jinengxia.com/session");
     	String srtResult = "";
     	try {
     		HttpResponse httpResponse = httpCilent.execute(httpGet);
